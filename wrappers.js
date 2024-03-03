54fed67f09a6eb6ca6422ed01c061696e92fe744
fujivara <marat.skyba@gmail.com>
2024-03-03 13:13:59 +0200

    Implemented text wrappers for corresponding elements

---
+++ wrappers.js
@@ -0,0 +1,20 @@
+export const paragraphWrapper = (text) => {
+  return `<p>${text}</p>`;
+};
+
+export const boldWrapper = (text) => {
+  return `<b>${text}</b>`;
+};
+
+export const italicWrapper = (text) => {
+  return `<i>${text}</i>`;
+};
+
+export const monospaceWrapper = (text) => {
+  return `<tt>${text}</tt>`;
+};
+
+export const preformattedWrapper = (text) => {
+  return `<pre>${text}</pre>`;
+};
+
